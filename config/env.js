5cd400b35f166d3edc897cf3d8a1defe498b2843
Pankaj Singh <pankajsingh8540@gmail.com>
2022-07-13 13:22:09 +0530

    updated config file

--- config/env.js
+++ config/env.js
@@ -1,10 +1,21 @@
 
+// const env = {
+//   database: "node_express_apis",
+//   username: "root",
+//   password: "root1234",
+//   host: "localhost",
+//   dialect: "mysql",
+// };
+
 const env = {
   database: "node_express_apis",
-  username: "root",
-  password: "root1234",
-  host: "localhost",
+  username: "pankaj@myserver213",
+  password: "Aabcd12@#",
+  host: "myserver213.mysql.database.azure.com",
   dialect: "mysql",
 };
 
 module.exports = env;
+
+
+
