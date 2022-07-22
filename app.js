6d37194e2495b249dacaa4929215947935bcc58e
Pankaj Singh <52364179+pankajsingh85@users.noreply.github.com>
2022-07-22 10:50:17 +0530

    Update app.js
    app commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); 
